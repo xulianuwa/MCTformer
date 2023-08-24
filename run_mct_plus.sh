0acc27ada87a5582053efb14648442d8644168aa
Lian Xu <87886359+xulianuwa@users.noreply.github.com>
2023-08-24 19:22:59 +1000

    Add files via upload

--- run_mct_plus.sh
+++ run_mct_plus.sh
@@ -6,14 +6,15 @@ python main.py  --data-path VOCdevkit/VOC2012 \
 python main.py --data-set VOC12MS \
                 --img-list VOCdevkit/VOC2012/ImageSets/Segmentation \
                 --data-path VOCdevkit/VOC2012 \
+                --gen_attention_maps \
                 --resume saved_model/checkpoint.pth \
 
 ######### Evaluating the generated class-specific localization maps ##########
 python evaluation.py --list voc12/train_id.txt \
                      --gt_dir VOCdevkit/VOC2012/SegmentationClassAug \
-                     --logfile cam-npy-folder/evallog.txt \
+                     --logfile cam-npy/evallog.txt \
                      --type npy \
                      --curve True \
-                     --predict_dir cam-npy-folder \
+                     --predict_dir cam-npy \
                      --comment "train1464"
 
